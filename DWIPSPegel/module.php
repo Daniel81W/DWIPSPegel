05d025bb58da4ac3901b6c5e5e43f19ee289f8a8
Daniel81W <kenya81@gmail.com>
2023-12-22 18:54:14 +0100

    Initial commit

--- DWIPSPegel/module.php
+++ DWIPSPegel/module.php
@@ -20,4 +20,29 @@ declare(strict_types=1);
 			//Never delete this line!
 			parent::ApplyChanges();
 		}
-	}
+
+        public function GetConfigurationForm()
+        {
+            $waters_URL = "https://pegelonline.wsv.de/webservices/rest-api/v2/waters.json";
+            $waters_json = file_get_contents($waters_URL);
+            $waters = json_decode($waters_json);
+
+            $i = 0;
+
+            $form = "";
+            $form .= "\"elements\": [";
+            $form .= "{\"type\": \"Select\",\"name\":\"water\",\"caption\":\"Gewässer\",\"options\":[";
+                foreach ($waters as $water){
+                    if($i>0){$form .= ",";}
+                    $form .= "{\"caption\": \"" . $i+1 . "\",\"value\":\"" . $i+1 . "\"}";
+                    $i++;
+                }
+            $form .= "]}";
+            $form .= "],";
+
+            $form .= "\"actions\": [],";
+
+            $form .= "\"status\": []";
+            return $form;
+        }
+    }
