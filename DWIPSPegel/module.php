f778251d4b903a2738a8bfa540f55f52c9a8fa0f
Daniel81W <kenya81@gmail.com>
2023-12-27 21:28:24 +0100

    Auf Properties umgestellt

--- DWIPSPegel/module.php
+++ DWIPSPegel/module.php
@@ -16,6 +16,7 @@ declare(strict_types=1);
             $this->RegisterAttributeString("unit", "");
             $this->RegisterAttributeInteger("interval", 0);
             $this->RegisterAttributeFloat("mthw", 0);
+            $this->RegisterPropertyFloat("mthw", 0);
 
             if(!IPS_VariableProfileExists("DWIPS.Pegel.Strecke.m")){
                 IPS_CreateVariableProfile("DWIPS.Pegel.Strecke.m", 2);
@@ -46,7 +47,7 @@ declare(strict_types=1);
 			parent::ApplyChanges();
 
             $this->SetTimerInterval("UpdateTimer", $this->ReadAttributeInteger("interval")*60000);
-            $this->SendDebug("Form", $mthw, 0);
+            //$this->SendDebug("Form", $mthw, 0);
             //$this->WriteAttributeFloat()
         }
 
